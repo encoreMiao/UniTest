66f2c406706d612bbb2bfd104fae3ac049f7c25d
sunxiaohan <xiaohansun@sohu-inc.com>
2016-12-30 14:59:11 +0800

    My first Commit

---
+++ SXHUniTest/SXHUniTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SXHUniTest
+//
+//  Created by sunxh on 16/12/30.
+//  Copyright © 2016年 sxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SXHUniTest/SXHUniTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SXHUniTest
+//
+//  Created by sunxh on 16/12/30.
+//  Copyright © 2016年 sxh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
